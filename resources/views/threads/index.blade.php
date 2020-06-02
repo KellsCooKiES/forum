60762c888f004b5b1bacf0eff5735c2be80fb68c
KellsCooKiES <51446767+KellsCooKiES@users.noreply.github.com>
2020-06-03 00:08:54 +0400

    basics for threads

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Forum Threads</div>
+
+                    <div class="card-body">
+                        @foreach($threads as $thread)
+                            <article class="p-2 ">
+                                <h4 class="pb-2"><a href="/threads/{{$thread->id}}">{{  $thread->title}}</a></h4>
+                                <p class="card-text">{{ $thread->body }}</p>
+                            </article>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
