5a511f64c02eb69778919f51336dfde580d30c02
dmitry.m <Uhhlove@yandex.ru>
2020-06-26 18:47:30 +0000

    flash component

---
+++ resources/js/components/flash.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="alert alert-warning fade show" role="alert">
+        <strong> Success!</strong> You successfully create a thread.
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
